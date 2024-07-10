0a5eb28bed5391c83794ad4870378f1a4492bd82
DevFaith <waitherafaith@outlook.com>
2024-07-10 20:47:17 +0300

    * feat(App.jsx): add App component with DataRead and Writedata components

---
+++ New folder/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import DataRead from "./component/DataRead";
+import Writedata from "./component/Writedata";
+
+function App() {
+
+  // read data 
+  return (
+    <>
+      <Writedata />
+      <DataRead />
+    </>
+  );
+}
+
+export default App;
+
+
